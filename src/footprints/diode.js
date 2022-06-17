0e9dd8d2aaa0908d07a76f860dcc91b75d8ced00
mroukema <mroukema@gmail.com>
2022-06-17 19:55:51 -0400

    Fix some issues

--- src/footprints/diode.js
+++ src/footprints/diode.js
@@ -1,48 +1,78 @@
+const Mode = {
+  Dual: 'Dual',
+  Smd: 'Smd',
+  Tht: 'Tht'
+}
+
 module.exports = {
     nets: {
         from: undefined,
         to: undefined
     },
     params: {
-        class: 'D'
+        class: 'D',
+        mode: Mode.Dual,
+        reverse: true
     },
-    body: p => `
-  
-    (module ComboDiode (layer F.Cu) (tedit 5B24D78E)
+    body: p => {
+      const [commonFront, commonBack] =
+        [`
+          ${''/* diode symbols front */}
+          (fp_line (start 0.25 0) (end 0.75 0) (layer F.SilkS) (width 0.1))
+          (fp_line (start 0.25 0.4) (end -0.35 0) (layer F.SilkS) (width 0.1))
+          (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer F.SilkS) (width 0.1))
+          (fp_line (start -0.35 0) (end 0.25 -0.4) (layer F.SilkS) (width 0.1))
+          (fp_line (start -0.35 0) (end -0.35 0.55) (layer F.SilkS) (width 0.1))
+          (fp_line (start -0.35 0) (end -0.35 -0.55) (layer F.SilkS) (width 0.1))
+          (fp_line (start -0.75 0) (end -0.35 0) (layer F.SilkS) (width 0.1))
+        `, `
+          ${''/* diode symbols back */}
+          (fp_line (start 0.25 0) (end 0.75 0) (layer B.SilkS) (width 0.1))
+          (fp_line (start 0.25 0.4) (end -0.35 0) (layer B.SilkS) (width 0.1))
+          (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer B.SilkS) (width 0.1))
+          (fp_line (start -0.35 0) (end 0.25 -0.4) (layer B.SilkS) (width 0.1))
+          (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
+          (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
+          (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
+        `];
+      const common = `
+        (module ComboDiode (layer F.Cu) (tedit 5B24D78E)
+          ${p.at /* parametric position */}
 
+          ${'' /* footprint reference */}
+          (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+          (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+      ` +
+        (p.param.reverse ? commonFront + commonBack : commonBack);
 
-        ${p.at /* parametric position */}
+      const tht = `
+          ${''/* THT terminals */}
+          (pad 1 thru_hole circle (at 3.81 0 ${p.rot}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.from.str})
+          (pad 2 thru_hole rect (at -3.81 0 ${p.rot}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.to.str})
+      `;
 
-        ${'' /* footprint reference */}
-        (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-        (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
-        
-        ${''/* diode symbols */}
-        (fp_line (start 0.25 0) (end 0.75 0) (layer F.SilkS) (width 0.1))
-        (fp_line (start 0.25 0.4) (end -0.35 0) (layer F.SilkS) (width 0.1))
-        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 0.55) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.75 0) (end -0.35 0) (layer F.SilkS) (width 0.1))
-        (fp_line (start 0.25 0) (end 0.75 0) (layer B.SilkS) (width 0.1))
-        (fp_line (start 0.25 0.4) (end -0.35 0) (layer B.SilkS) (width 0.1))
-        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
-        (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
-    
-        ${''/* SMD pads on both sides */}
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.from.str})
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.from.str})
-        
-        ${''/* THT terminals */}
-        (pad 1 thru_hole circle (at 3.81 0 ${p.rot}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.from.str})
-        (pad 2 thru_hole rect (at -3.81 0 ${p.rot}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.to.str})
-    )
-  
-    `
+      const [smdFront, smdBack] =
+        [`
+          ${''/* SMD pads on front side */}
+          (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.to.str})
+          (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.from.str})
+        `, `
+          ${''/* SMD pads on back side */}
+          (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.from.str})
+          (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.to.str})
+        `];
+      const smd = p.param.reverse ? smdFront + smdBack : smdBack;
+      const suffix = `
+        )
+      `;
+
+      switch (p.param.mode) {
+        case Mode.Dual:
+          return common + smd + tht + suffix;
+        case Mode.Smd:
+          return common + smd + suffix;
+        case Mode.Tht:
+          return common + tht + suffix;
+      }
+    }
 }
